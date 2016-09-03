f41f7f9cbcbb98803dbf0495217ead4c604bee6d
sbtjavaschool <sbt.java.school@gmail.com>
2016-09-03 13:19:56 +0300

    .

--- src/ThreadPool.java
+++ src/ThreadPool.java
@@ -2,8 +2,25 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ThreadPool {
-    private final BlockingQueue<Runnable> queue
-            = new LinkedBlockingQueue<>();
+    private final Runnable poisonPill = () -> {
+    };
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) {
+        ThreadPool pool = new ThreadPool();
+        pool.add(null);
+        pool.add(null);
+        pool.add(null);
+
+        //end
+        pool.usePoisonPill();
+    }
+
+    private void usePoisonPill() {
+        add(poisonPill);
+        add(poisonPill);
+        add(poisonPill);
+    }
 
     public void add(Runnable runnable) {
         queue.add(runnable);
@@ -12,9 +29,16 @@ public class ThreadPool {
     public void worker() {
         try {
             Runnable task = queue.take();
+            if (task == poisonPill) {
+
+            }
+
             task.run();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        System.out.println();
+        //logic
     }
 }
